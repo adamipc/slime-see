a860591b3f1272b7ca1b1924bb530bde26d2e04c
Adam Wendt <thelsdj@gmail.com>
2023-04-25 18:48:56 -0700

    indices

--- code/gl/gl_definitions.h
+++ code/gl/gl_definitions.h
@@ -1,14 +1,39 @@
 #ifndef GL_DEFINITIONS_H
 #define GL_DEFINITIONS_H
 
+
+#define GL_FALSE                          0
+#define GL_TRIANGLES                      0x0004
+
+#define GL_FRONT_AND_BACK                 0x0408
+
+#define GL_FLOAT                          0x1406
+#define GL_UNSIGNED_INT                   0x1405
+
+#define GL_LINE                           0x1B01
+
+#define GL_VERSION                        0x1F02
+
 #define GL_COLOR_BUFFER_BIT               0x00004000
 
 #define GL_ARRAY_BUFFER                   0x8892
+#define GL_ELEMENT_ARRAY_BUFFER           0x8893
+
 #define GL_STATIC_DRAW                    0x88E4
 
-#define GL_VERSION                        0x1F02;
+#define GL_FRAGMENT_SHADER                0x8B30
+#define GL_VERTEX_SHADER                  0x8B31
+
+#define GL_COMPILE_STATUS                 0x8B81
+#define GL_LINK_STATUS                    0x8B82
+
+#define GL_NO_ERROR                       0
 
 typedef int           GLsizei;
+typedef int           GLint;
+typedef bool          GLboolean;
+typedef char          GLchar;
+typedef float         GLfloat;
 typedef unsigned int  GLenum;
 typedef unsigned int  GLuint;
 typedef unsigned char GLubyte;
--- code/win32_slime_see.cpp
+++ code/win32_slime_see.cpp
@@ -72,30 +72,131 @@ WinMain(HINSTANCE Instance,
   ShowWindow(window.window, SW_SHOW);
 
   float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+    -0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
   };
+
+  unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3,
+  };
+
+  const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec4 aColor;\n"
+    "out vec4 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "  gl_Position = vec4(aPos, 1.0);\n"
+    "  ourColor = aColor;\n"
+    "}\0";
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  int success;
+  char infoLog[512];
+
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s\n", infoLog);
+  }
+
+  const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "in vec4 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "  FragColor = ourColor;\n"
+    "}\n\0";
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", infoLog);
+  }
+
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n", infoLog);
+  }
+
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
 
   GLuint vbo;
   glGenBuffers(1, &vbo);
+
+  GLuint ebo;
+  glGenBuffers(1, &ebo);
+
+  glBindVertexArray(vao);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  // color attribute
+  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  GLenum error_code = GL_NO_ERROR;
+  glBindVertexArray(0);
+
+  //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  float u_time = 0.f;
+
   for(;;) {
     MSG msg = {};
-    BOOL get_message = GetMessage(&msg, 0, 0, 0);
-    if (get_message == 0) {
-      break;
+    while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
     }
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
 
     HDC dc = GetDC(window.window);
     win32_wglMakeCurrent(dc, window.glrc);
 
     glClearColor(0.9f, 0.3f, 0.f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    float greenValue = (sin(u_time) / 2.0f) + 0.5f;
+    //int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+    glUseProgram(shaderProgram);
+    //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    error_code = glGetError();
+    if (error_code != GL_NO_ERROR) {
+      printf("ERROR::OPENGL::%d\n", error_code);
+    }
+
+    u_time += 0.01f;
 
     SwapBuffers(dc);
     ReleaseDC(window.window, dc);
