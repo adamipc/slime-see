b791f33bffeb5d5dbd4efadfb228b81878b9b192
Adam Wendt <adam@awendtconsulting.com>
2023-04-30 12:30:18 -0700

    removed required sdk version from shell.bat

--- misc/shell.bat
+++ misc/shell.bat
@@ -1,6 +1,6 @@
 @echo off
 
-CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64 10.0.22621.0
+CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
 set PATH=%PATH%;W:\handmade\misc\
 
